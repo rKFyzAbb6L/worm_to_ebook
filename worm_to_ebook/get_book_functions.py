0e72698ead54f6471277985b136f8c33230df8a7
rKFyzAbb6L <rKFyzAbb6L>
2021-02-24 20:17:16 -0700

    Change get_book to recursive and prettify content
    it works. huzah.
    simple json dump if called directly. should be able to use any individual chapter as entry point to get that and following chapters.
    No backtracking through previous chapters via prevchapter link

--- worm_to_ebook/get_book_functions.py
+++ worm_to_ebook/get_book_functions.py
@@ -33,38 +33,40 @@ def parse_entry_content(html):
     return entry
 
 
-def parse_chapter(chapter_html):
-    nextchapter = get_next_chapter_link(chapter_html)
-    prevchapter = get_last_chapter_link(chapter_html)
+def parse_chapter(chapter_url, chapter_html):
+    next_chapter = get_next_chapter_link(chapter_html)
+    prev_chapter = get_last_chapter_link(chapter_html)
     entry = parse_entry_content(chapter_html)
     for div in entry['content'].find_all('div', id="jp-post-flair"):
         div.decompose()
     for a in entry['content'].find_all('a'):
         a.decompose()
     entry['content'].smooth()
-    chapter_dict = {'chapterTitle': entry['title'],
-                    'chapterContent': entry['content'],
-                    'nextchapter': nextchapter,
-                    'prevchapter': prevchapter}
+    chapter_dict = {'chapterTitle': str(entry['title']),
+                    'chapterUrrl': str(chapter_url),
+                    'chapterContent': entry['content'].prettify(
+                                                       formatter='html'),
+                    'nextChapter': str(next_chapter),
+                    'prevChapter': str(prev_chapter)}
     return chapter_dict
 
 
 def get_chapter(chapter_url):
     chapter_html = get_chapter_html(chapter_url)
-    chapter = parse_chapter(chapter_html)
+    chapter = parse_chapter(chapter_url, chapter_html)
     return chapter
 
 
 def get_book(chapter_url):
-    book = {}
-    while(True):
-        chapter = get_chapter(chapter_url)
-        book[chapter_url] = chapter
-        chapter_url = chapter['nextchapter']
-        if None is chapter_url:
-            break
-    return book
+    chapter = get_chapter(chapter_url)
+    if 'None' == chapter['nextChapter']:
+        return [chapter]
+    return [chapter] + get_book(chapter['nextChapter'])
 
 
-if __name__ == '__main__':
-    book = get_book('https://parahumans.wordpress.com/2013/10/15/speck-30-1/')
+if __name__ == "__main__":
+    import json
+    book = get_book('https://parahumans.wordpress.com' +
+                    '/category/stories-arcs-1-10/arc-1-gestation/1-01/')
+    with open('worm.json', 'w') as f:
+        json.dump({'book': book}, f)
