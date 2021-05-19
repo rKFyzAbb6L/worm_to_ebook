2614e0545037ce3bfadc46d79e89bc9887faf7f6
rKFyzAbb6L <rKFyzAbb6L>
2021-05-18 21:22:58 -0700

    Fix typo in output filename

--- make_worm_epub.py
+++ make_worm_epub.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     worm_book = get_book('https://parahumans.wordpress.com' +
                          '/category/stories-arcs-1-10/arc-1-gestation/1-01/')
     worm_epub = make_epub(worm_book)
-    epub.write_epub('worm_by_jc_mcrae.epub', worm_epub, {})
+    epub.write_epub('worm_by_jc_mccrae.epub', worm_epub, {})
